be8a01cccfef0614e12548113641149572bc73ec
Sir Mino <115249835+SirVoly@users.noreply.github.com>
2026-01-22 14:34:51 +0000

    First few end points

---
+++ internal/server/apiconfig.go
@@ -0,0 +1,31 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) showMetricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hits: %d", cfg.fileserverHits.Load())
+}
+
+func (cfg *apiConfig) resetMetricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Metrics reset")
+	cfg.fileserverHits.Store(0)
+}
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,9 +1,29 @@
 package server
 
 import (
-	"fmt"
+	"net/http"
 )
 
 func Run() {
-	fmt.Println("Hi")
+	serverMux := http.NewServeMux()
+	apiCfg := apiConfig{}
+
+	// A link with the suffix /app/ will be redirected to this handler, which in turn will remove the /app/ prefix, so the FileServer can interpret this in the same way as the files.
+	serverMux.Handle("/app/", apiCfg.middlewareMetricsInc(middlewareLog(http.StripPrefix("/app/", http.FileServer(http.Dir("."))))))
+
+	serverMux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	serverMux.HandleFunc("GET /metrics", apiCfg.showMetricsHandler)
+	serverMux.HandleFunc("POST /reset", apiCfg.resetMetricsHandler)
+
+	server := http.Server{
+		Handler: serverMux,
+		Addr: ":8080",
+	}
+
+	server.ListenAndServe()
 }
